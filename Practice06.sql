be6e6022f6ff268241e74ca19349b3e0c407fe5a
Sayan Chandra <iamsayan2802@gmail.com>
2026-08-30 22:18:56 +0530

    Refactor Practice06.sql for improved formatting and add queries to select female employees based on salary comparison with male employees

--- Practice06.sql
+++ Practice06.sql
@@ -1,21 +1,48 @@
+-- SQLBook: Code
 CREATE TABLE Employee (
     Eid VARCHAR(10) PRIMARY KEY,
     Salary DECIMAL(10, 2) NOT NULL,
     Gender CHAR(1)
 );
 
-INSERT INTO Employee (Eid, Salary, Gender) VALUES 
-('E1', 50000.00, 'M'),
-('E2', 60000.00, 'F'),
-('E3', 55000.00, 'M'),
-('E4', 70000.00, 'F'),
-('E5', 65000.00, 'M'),
-('E6', 72000.00, 'F'),
-('E7', 58000.00, 'M'),
-('E8', 75000.00, 'F'),
-('E9', 62000.00, 'M'),
-('E10', 80000.00, 'F');
+INSERT INTO
+    Employee (Eid, Salary, Gender)
+VALUES ('E1', 50000.00, 'M'),
+    ('E2', 60000.00, 'F'),
+    ('E3', 55000.00, 'M'),
+    ('E4', 70000.00, 'F'),
+    ('E5', 65000.00, 'M'),
+    ('E6', 72000.00, 'F'),
+    ('E7', 58000.00, 'M'),
+    ('E8', 75000.00, 'F'),
+    ('E9', 62000.00, 'M'),
+    ('E10', 80000.00, 'F');
 
+SELECT Eid
+FROM Employee
+WHERE
+    Gender = 'F'
+    AND Salary > ANY (
+        SELECT Salary
+        FROM Employee
+        WHERE
+            Gender = 'M'
+    );
 
+SELECT Eid
+FROM Employee
+WHERE
+    Gender = 'F'
+    AND Salary > (
+        SELECT MIN(Salary)
+        FROM Employee
+        WHERE
+            Gender = 'M'
+    );
 
-
+SELECT DISTINCT E1.Eid
+FROM Employee E1, Employee E2
+WHERE
+    E1.Gender = 'F'
+    AND E2.Gender = 'M'
+    AND E1.Salary > E2.Salary;
